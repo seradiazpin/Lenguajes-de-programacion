dea477fbf32b9efd6c3ea781a848fbdd709fe336
Sergio Alejandro Diaz Pinilla <seradiazpin@unal.edu.co>
2016-03-18 00:14:59 -0500

    Arreglado error de """"  comillas dobles seguidas :D

--- AnalisisLexico/jejhernandezpa-seradiazpin.py
+++ AnalisisLexico/jejhernandezpa-seradiazpin.py
@@ -56,7 +56,7 @@ class DictionaryRegExp:
                       "leer": "leer", "si": "si", "entonces": "entonces", "fin_si": "fin_si", "si_no": "si_no", "mientras": "mientras", "hacer": "hacer", "fin_mientras": "fin_mientras",
                       "para": "para", "fin_para": "fin_para", "seleccionar": "seleccionar", "entre": "entre", "caso": "caso", "romper": "romper", "defecto": "defecto",
                       "fin_seleccionar": "fin_seleccionar", "verdadero":"verdadero", "falso":"falso", "fin_funcion":"fin_funcion"}
-    lex_token = {"-?\d+":"tk_entero", "-?\d+\.\d+": "tk_real", "\".*\"": "tk_cadena", "\'(.| )\'": "tk_caracter", "[a-zA-Z][\w_-]*": "id"}
+    lex_token = {"-?\d+":"tk_entero", "-?\d+\.\d+": "tk_real", "\".*\"": "tk_cadena", "\'.\'": "tk_caracter", "[a-zA-Z][\w_-]*": "id"}
     TOKEN = 0
     LEX_TOKEN = 1
     MAX_SIZE_SIMPLE_TOKEN = 2
@@ -66,8 +66,10 @@ class DictionaryRegExp:
         self.current_lexer = ""
         self.start_line_lexer = 0
         self.start_column_lexer = 0
-        self.STRING = False;
-        self.COMMENT = False;
+        self.STRING = False
+        self.COMMENT = False
+        self.CHAR = False
+        self.STREND = False
 
 
         self.tokens = []
@@ -112,10 +114,23 @@ class DictionaryRegExp:
                 self.current_lexer = ""
                 continue
 
+
             # string mode
             if char == '\"' or self.STRING:
                 if char == '\"':
+                    if self.STRING:
+                        self.STREND = True
                     self.STRING = not self.STRING
+                if not self.STREND:
+                    self.current_lexer += char
+                    continue
+                else:
+                    self.current_lexer += char
+
+            #char mode
+            if char == '\'' or self.CHAR:
+                if char == '\'':
+                    self.CHAR = not self.CHAR
                 self.current_lexer += char
                 continue
 
@@ -125,7 +140,7 @@ class DictionaryRegExp:
                 self.current_lexer = ""
                 continue
 
-            if(char in end_tokens or char in self.simple_token):
+            if(char in end_tokens or char in self.simple_token or char =="\""):
 
                 if not self.COMMENT:
                     try:
@@ -145,8 +160,11 @@ class DictionaryRegExp:
                     print result[self.TOKEN]
                     self.tokens.append(str(result[self.TOKEN])+"\n")
 
-                if(result[self.LEX_TOKEN]):
+                if result[self.LEX_TOKEN] and not self.STREND:
                     proc_prog.backward()
+                else:
+                    self.STREND = False
+
                 self.start_line_lexer = self.proc_prog.current_line
                 self.start_column_lexer = self.proc_prog.current_column
                 self.current_lexer = ""
@@ -195,8 +213,8 @@ def read_file(file_name):
         lines[i] = lines[i].replace("\r","")
     return lines
 
-n = 7
-l = "A"
+n = 4
+l = "C"
 file_init = "./problemas_juez/L1"+l+"_2016_"+str(n)
 
 program = []
@@ -220,7 +238,7 @@ for i in range(len(tokens_list)):
     try:
         assert out[i] == tokens_list[i]
     except AssertionError:
-        print ">>> Error " + tokens_list[i]+" != "+out[i]
+        print ">>> Error " + tokens_list[i]+" != "+out[i]+" >>Linea "+str(i+1)
 
 
 
